e44bc1753236de60afb50dfb23f13fb6599c44e3
Alish1660 <git>
2024-09-04 22:07:32 +0500

    change

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const weddingDate = new Date("Sentabr 15, 2024 06:43:00").getTime();
+const weddingDate = new Date("September 15, 2024 06:43:00").getTime();
 
 function updateCountdown() {
   const now = new Date().getTime();
   const distance = weddingDate - now;
+
+  if (distance < 0) {
+    clearInterval(interval);
+    document.querySelector(".countdown").innerHTML = "To'y boshlandi!";
+    return;
+  }
+
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -10,17 +17,14 @@ function updateCountdown() {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days + " <span>kun</span>";
-  document.getElementById("hours").innerHTML = hours + " <span>soat</span>";
-  document.getElementById("minutes").innerHTML =
-    minutes + " <span>daqiqalar</span>";
-  document.getElementById("seconds").innerHTML =
-    seconds + " <span>soniyalar</span>";
-
-  if (distance < 0) {
-    clearInterval(interval);
-    document.querySelector(".countdown").innerHTML = "To'y boshlandi!";
-  }
+  document.getElementById("days").innerHTML = `${days} <span>kun</span>`;
+  document.getElementById("hours").innerHTML = `${hours} <span>soat</span>`;
+  document.getElementById(
+    "minutes"
+  ).innerHTML = `${minutes} <span>daqiqalar</span>`;
+  document.getElementById(
+    "seconds"
+  ).innerHTML = `${seconds} <span>soniyalar</span>`;
 }
 
 const interval = setInterval(updateCountdown, 1000);
